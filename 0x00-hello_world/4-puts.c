b86caa69166a75abda63be001e072b151e034da7
root <6154@holbertonstudents.com>
2023-02-08 18:09:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  * puzzle, followed by a newline.
  * return 0.
  */
- int main(void)
- {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle")
+return (0);
 }
