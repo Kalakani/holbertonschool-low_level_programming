4d0fd78efa1a8bfd0a3c8772c6858ebcc9c9924c
root <6154@holbertonstudents.com>
2023-02-08 18:14:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * return 0.
- */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle")
-return (0);
+ */                                         int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzlei")
+		return (0);
 }
